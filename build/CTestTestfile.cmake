f095a602b69d0920e990a9ec5ebda619d7006373
Shane Skikne <shane@shane-Latitude-E6420.(none)>
2014-09-28 19:32:35 -0400

    Setup the folders

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/shane/catkin_ws/src/MobileRobotics/src
+# Build directory: /home/shane/catkin_ws/src/MobileRobotics/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
